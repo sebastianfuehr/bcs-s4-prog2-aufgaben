a796b9eaa3c9330afa37cb36a9462b0c695c7d3c
KrokodileDandy <s.fuehr1@gmail.com>
2019-07-24 10:22:25 +0200

    Solved homework 6

--- Hausaufgaben_Wiederholung/homework_6/template/src/main/scala/movies/Movies.scala
+++ Hausaufgaben_Wiederholung/homework_6/template/src/main/scala/movies/Movies.scala
@@ -1,5 +1,7 @@
 package movies
 
+import scala.collection.mutable.ListBuffer
+
 object Movies {
   /**
     * Example: Returns a list of users with long names
@@ -19,7 +21,11 @@ object Movies {
     * @param movies the list of movies
     * @return a list of movies that were released after the year 2000
     */
-  def findAllMoviesAfter2000(movies: List[Movie]): List[String] = ???
+  def findAllMoviesAfter2000(movies: List[Movie]): List[String] = {
+    val movieTitles: ListBuffer[String] = ListBuffer()
+    for (movie <- movies) if (movie.year >= 2000) movieTitles += movie.name
+    movieTitles.toList
+  }
 
   /**
     * Return all movies with titles starting with "The"
@@ -27,7 +33,11 @@ object Movies {
     * @param movies
     * @return
     */
-  def moviesStartingWithThe(movies: List[Movie]): List[String] = ???
+  def moviesStartingWithThe(movies: List[Movie]): List[String] = {
+    val moviesList: ListBuffer[String] = ListBuffer()
+    for (movie <- movies) if (movie.name.startsWith("The")) moviesList += movie.name
+    moviesList.toList
+  }
 
   val theGodfather = "The Godfather"
   /**
@@ -37,5 +47,12 @@ object Movies {
     * @param movies a list of movies
     * @return a List of Pairs of the user Id and the godfathers's movie Id
     */
-  def whoOwnsTheGodfather(users: List[User], movies: List[Movie]): List[(Int, Int)] = ???
+  def whoOwnsTheGodfather(users: List[User], movies: List[Movie]): List[(Int, Int)] = {
+    val theGodfatherId = 1
+    for {user <- users
+         if user.moviesIdOwned.contains(theGodfatherId)
+         movie <- movies
+         if movie.name == theGodfather
+    } yield (user.id, theGodfatherId)
+  }
 }
