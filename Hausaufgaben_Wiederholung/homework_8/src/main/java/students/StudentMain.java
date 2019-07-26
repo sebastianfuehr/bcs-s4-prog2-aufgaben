81c133a3915ab7f0da173c509fdf4556020f430c
KrokodileDandy <s.fuehr1@gmail.com>
2019-07-26 14:27:40 +0200

    Solved homework 8 completely

--- Hausaufgaben_Wiederholung/homework_8/src/main/java/students/StudentMain.java
+++ Hausaufgaben_Wiederholung/homework_8/src/main/java/students/StudentMain.java
@@ -11,47 +11,55 @@ import java.util.stream.Stream;
 public class StudentMain {
 
 
-	public static void main(String[] args) {
-		List<Student> students = generateStudents();
-	
-		/**
-		* TODO:  
-		* 
-		*/
+    public static void main(String[] args) {
+        List<Student> students = generateStudents();
+
+		System.out.println("Filter for 'Wi-Inf' and 4.0 or better:");
+		students.stream().filter(StudentMain::byWiInfs).forEach(System.out::println);
+
+		System.out.println("Best student: " + students.stream().reduce(StudentMain::reduceFunction).get());
+    }
+
+    public static boolean byWiInfs(Student student) {
+    	return student.studymajor.equals("Wi-Inf") && student.grade <= 4.0;
 	}
 
-public static List<Student> generateStudents() {
-		List<Student> students = new ArrayList<Student>();
-		Student student = new Student("John", "Doe","Wi-Inf",1.0);
-		Student student1 = new Student("Jack", "Doe", "Wi-Inf", 5.0);
-		Student student2 = new Student("Chuck", "Norris", "Everything", 0.9);
-		Student student3 = new Student("John", "Hancock", "DrugPrevention", 1.3);
-		Student student4 = new Student("Peter", "Pan", "PiracyStudies", 3.7);
-		Student student5 = new Student("Tony", "Stark", "ElectricalEngineering", 1.7);
-		Student student6 = new Student("Peter", "Parker", "Biology", 5.0);
-		Student student7 = new Student("Bruce", "Wayne", "BusinessAdministration", 4.0);
-		Student student8 = new Student("Clark", "Kent", "Journalism", 1.0);
-		Student student9 = new Student("Logan", "Howlett", "IndustrialDesign", 2.7);
-		Student student10 = new Student("Robert Bruce", "Banner", "Physics", 5.0);
-		Student student11 = new Student("Arthur", "Dent", "Ultimate Question of Life, the Universe, and Everything",42.0);
-		Student student12 = new Student("Alice", "Wonderland", "Wi-Inf", 2.0);
-		Student student13 = new Student("Bob", "Wonderland", "Wi-Inf", 3.0);
-		
-		students.add(student);
-		students.add(student1);
-		students.add(student2);
-		students.add(student3);
-		students.add(student4);
-		students.add(student5);
-		students.add(student6);
-		students.add(student7);
-		students.add(student8);
-		students.add(student9);
-		students.add(student10);
-		students.add(student11);
-		students.add(student12);
-		students.add(student13);
-		return students;
+	public static Student reduceFunction(Student s1, Student s2) {
+		return (s1.grade < s2.grade) ? s1 : s2;
 	}
-	
+
+    public static List<Student> generateStudents() {
+        List<Student> students = new ArrayList<Student>();
+        Student student = new Student("John", "Doe", "Wi-Inf", 1.0);
+        Student student1 = new Student("Jack", "Doe", "Wi-Inf", 5.0);
+        Student student2 = new Student("Chuck", "Norris", "Everything", 0.9);
+        Student student3 = new Student("John", "Hancock", "DrugPrevention", 1.3);
+        Student student4 = new Student("Peter", "Pan", "PiracyStudies", 3.7);
+        Student student5 = new Student("Tony", "Stark", "ElectricalEngineering", 1.7);
+        Student student6 = new Student("Peter", "Parker", "Biology", 5.0);
+        Student student7 = new Student("Bruce", "Wayne", "BusinessAdministration", 4.0);
+        Student student8 = new Student("Clark", "Kent", "Journalism", 1.0);
+        Student student9 = new Student("Logan", "Howlett", "IndustrialDesign", 2.7);
+        Student student10 = new Student("Robert Bruce", "Banner", "Physics", 5.0);
+        Student student11 = new Student("Arthur", "Dent", "Ultimate Question of Life, the Universe, and Everything", 42.0);
+        Student student12 = new Student("Alice", "Wonderland", "Wi-Inf", 2.0);
+        Student student13 = new Student("Bob", "Wonderland", "Wi-Inf", 3.0);
+
+        students.add(student);
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        students.add(student4);
+        students.add(student5);
+        students.add(student6);
+        students.add(student7);
+        students.add(student8);
+        students.add(student9);
+        students.add(student10);
+        students.add(student11);
+        students.add(student12);
+        students.add(student13);
+        return students;
+    }
+
 }
--- Hausaufgaben_Wiederholung/homework_8/src/test/java/streams/StreamsTest.java
+++ Hausaufgaben_Wiederholung/homework_8/src/test/java/streams/StreamsTest.java
@@ -2,6 +2,10 @@ package streams;
 
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class StreamsTest {
@@ -9,12 +13,72 @@ public class StreamsTest {
     @Test
     public void testOrder() {
         Stream.of("d2", "a2", "b1", "b3", "c", "a6")
+                .filter(s -> { System.out.println("filter: " + s); return s.startsWith("a");
+                })
+                .limit(2)
                 .map(s -> {
                     System.out.println("map: " + s);
                     return s.toUpperCase(); })
-                .filter(s -> { System.out.println("filter: " + s); return s.startsWith("A");
-                })
-                .limit(2)
                 .forEach(s -> System.out.println("forEach: " + s));
+    }
+
+    @Test
+    public void paralellVsSequentialStreamBenchmarkTest() {
+        final int amountOfTests = 5;
+        System.out.println("Test\t Sequential\t Paralell");
+        int timeSeq = 0;
+        int timePar = 0;
+        for (int i=0; i<amountOfTests; i++) {
+            long seq = testSequentialStream();
+            long par = testParalellStream();
+            timeSeq += seq;
+            timePar += par;
+            System.out.println("\t" + i + "\t\t\t" + seq + "\t\t\t" + par);
+        }
+        System.out.println("Avg.:\t\t\t" + (timeSeq/amountOfTests) + "\t\t\t" + (timePar/amountOfTests));
+    }
+
+    private long testSequentialStream() {
+        long start = 0L;
+        long end = 0L;
+        try {
+            List<Record> list = Citizens.citizenList();
+            start = System.currentTimeMillis();
+            long amount = list.stream().filter(r -> {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    System.out.println("Process was interrupted.");
+                    e.printStackTrace();
+                }
+                return (r.geschlecht.equals("2"));
+            }).count();
+            end = System.currentTimeMillis();
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return end - start;
+    }
+
+    private long testParalellStream() {
+        long start = 0L;
+        long end = 0L;
+        try {
+            List<Record> list = Citizens.citizenList();
+            start = System.currentTimeMillis();
+            long amount = list.parallelStream().filter(r -> {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    System.out.println("Process was interrupted.");
+                    e.printStackTrace();
+                }
+                return (r.geschlecht.equals("2"));
+            }).count();
+            end = System.currentTimeMillis();
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return end - start;
     }
 }
