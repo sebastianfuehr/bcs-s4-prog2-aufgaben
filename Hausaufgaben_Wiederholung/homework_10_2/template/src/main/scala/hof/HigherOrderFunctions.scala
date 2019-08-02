75e6cc147cdb0a9505b724a8f421981c8dfddfb1
KrokodileDandy <s.fuehr1@gmail.com>
2019-08-02 09:43:44 +0200

    Resolving vulnerability in Klausurvorbereitung homework 10 2

--- Hausaufgaben_Wiederholung/homework_10_2/template/src/main/scala/hof/HigherOrderFunctions.scala
+++ Hausaufgaben_Wiederholung/homework_10_2/template/src/main/scala/hof/HigherOrderFunctions.scala
@@ -1,5 +1,22 @@
 package hof
 
+import scala.util.Random
+
 object HigherOrderFunctions {
-  // TODO
+
+  def main(args: Array[String]): Unit = {
+    val a: Int = new Random().nextInt(1000)
+    val b: Int = new Random().nextInt(1000)
+    println(s"Higher number of $a and $b: ${greaterFun(a, b, greater)}")
+
+
+  }
+
+  val greater: (Int, Int) => Int = (a, b) => if (a > b) a else b
+
+  // Alles zwischen ':' und '=' kann weggelassen werden
+  val greaterFun: (Int, Int, (Int, Int) => Int) => Int = (a: Int, b: Int, fun: (Int, Int) => Int) => fun(a, b)
+
+  val product: Int => (Int => Int) = (x: Int) => (y: Int) => x * y
+
 }
